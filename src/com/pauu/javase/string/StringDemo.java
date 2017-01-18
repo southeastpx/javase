57cf8983d7f201aa3fd3e6cc49d508a87d24da0c
peng.xing <southeast_px@163.com>
2017-01-18 15:20:04 +0800

    Scanner、String类常用API

---
+++ src/com/pauu/javase/string/StringDemo.java
@@ -0,0 +1,38 @@
+package com.pauu.javase.string;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		// String s1 = new String();
+		// String s2 = "abc";
+		// System.out.println("s2:"+s2);
+		// System.out.println("s1:" + s1.toString());
+		// System.out.println("length:" + s1.length());
+		// System.out.println("---------------------");
+		// byte[] bytes = {97,98,99,100,101};
+		// String s3 = new String(bytes);
+		// System.out.println("s3:"+s3);
+		//
+		// String s4 = new String(bytes,1,3);
+		// System.out.println("s4:"+s4);
+		//
+		// System.out.println("--------------");
+		// char[] chs = {'a','b','c','d','e'};
+		// String s5 = new String(chs);
+		// System.out.println("s5:"+s5);
+		// String s6 = new String(chs,1,3);
+		// System.out.println("s6:"+s6);
+
+		// System.out.println("--------------");
+
+		// String s = new String("abcde");
+		//
+		// System.out.println("---------");
+		// String s = "hello";
+		// s += "world";
+		// System.out.println("s"+s);
+		String s1 = new String("hello");
+		String s2 = "hello";
+		System.out.println(s1 == s2);//false
+		System.out.println(s1.equals(s2));//true
+	}
+}
