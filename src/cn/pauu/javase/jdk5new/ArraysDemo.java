458cbe7a1fb2c957b92de41ecb92ba479dc244f5
peng.xing <southeast_px@163.com>
2017-02-10 16:25:11 +0800

    Arrays.asList(T[]...arr)详细分析

--- src/cn/pauu/javase/jdk5new/ArraysDemo.java
+++ src/cn/pauu/javase/jdk5new/ArraysDemo.java
@@ -5,19 +5,19 @@ import java.util.List;
 
 public class ArraysDemo {
 	public static void main(String[] args) {
-		//String[] arrString = {"hello","world","java"};
-		int[] arrInt = {1,2,3};
-//		Integer[] arrInt = {1,2,3};
+		// String[] arrString = {"hello","world","java"};
+		int[] arrInt = { 1, 2, 3 };
+		// Integer[] arrInt = {1,2,3};
 		List<int[]> list = Arrays.asList(arrInt);
-		//UnsupportedOperationException,本质还是一个数组，不能改变数组的长度
-		//list.add("andriod");
-		//UnsupportedOperationException,本质还是一个数组，不能改变数组的长度
-		//list.remove(1);
+		// UnsupportedOperationException,本质还是一个数组，不能改变数组的长度
+		// list.add("andriod");
+		// UnsupportedOperationException,本质还是一个数组，不能改变数组的长度
+		// list.remove(1);
 		// for(Integer s:list){
 		// System.out.println(s);
 		// }
-		for(int[] s:list){
-			for(int x : s){
+		for (int[] s : list) {
+			for (int x : s) {
 				System.out.println(x);
 			}
 		}
