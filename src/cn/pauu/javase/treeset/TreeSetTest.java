be9cec5cac62d39729cb4b27597300a9ed9eed37
peng.xing <southeast_px@163.com>
2017-02-17 09:48:21 +0800

    单列集合Collection总结

--- src/cn/pauu/javase/treeset/TreeSetTest.java
+++ src/cn/pauu/javase/treeset/TreeSetTest.java
@@ -3,7 +3,30 @@ package cn.pauu.javase.treeset;
 import java.util.Comparator;
 import java.util.Scanner;
 import java.util.TreeSet;
-
+/*
+ *     Collection
+ *         |--List(有序，可以重复)
+ *             |--ArrayList
+ *             		底层数据结构是数组，查询快，增删慢
+ *             		线程不安全，高效
+ *             |--LinkedList
+ *             		底层数据结构是链表，查询慢，增删快
+ *             		线程不安全，高效
+ *             |--Vector
+ *             		底层数据结构是数组，查询快，增删慢
+ *             		线程安全，低效
+ *         |--Set(无序，唯一)
+ *         	   |--HashSet
+ *         			底层数据结构是哈希表（挂着链表的数组）
+ *         			保证唯一：通过元素的hashCode()和equals()方法
+ *         			|--LinkedHashSet
+ *         				底层数据结构是链表和哈希表，链表保证有序，哈希表保证唯一
+ *         	   |--TreeSet
+ *         			底层数据界都是二叉树。
+ *         			排序有两种：①自然排序（元素具有比较性）-->元素类实现Comparable接口，重写compareTo()方法
+ *         					  ②比较器排序（集合具有比较性）-->自定义比较器类实现Comparator接口，重写compare()方法
+ *         			保证唯一：通过重写方法的返回值是否为0判断
+ */
 public class TreeSetTest {
 	public static void main(String[] args) {
 		TreeSet<Teacher> treeSet = new TreeSet<Teacher>(new Comparator<Teacher>() {
@@ -23,26 +46,27 @@ public class TreeSetTest {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("请输入第" + i + "个学生的姓名：");
 			String name = scanner.nextLine();
-			System.out.println("请输入第"+i+"个学生的语文成绩");
+			System.out.println("请输入第" + i + "个学生的语文成绩");
 			int chinese = scanner.nextInt();
-			System.out.println("请输入第"+i+"个学生的数学成绩");
+			System.out.println("请输入第" + i + "个学生的数学成绩");
 			int math = scanner.nextInt();
-			System.out.println("请输入第"+i+"个学生的英语成绩");
+			System.out.println("请输入第" + i + "个学生的英语成绩");
 			int english = scanner.nextInt();
-			
+
 			Teacher teacher = new Teacher();
 			teacher.setName(name);
 			teacher.setChinese(chinese);
 			teacher.setMath(math);
 			teacher.setEnglish(english);
-			
+
 			treeSet.add(teacher);
 		}
 		System.out.println("键盘录入结束！");
 		System.out.println("总分从高到低输出学生信息");
 		System.out.println("学生姓名\t语文成绩\t数学成绩\t英语成绩");
-		for(Teacher teacher:treeSet){
-			System.out.println(teacher.getName()+"\t"+teacher.getChinese()+"\t"+teacher.getMath()+"\t"+teacher.getEnglish());
+		for (Teacher teacher : treeSet) {
+			System.out.println(teacher.getName() + "\t" + teacher.getChinese() + "\t" + teacher.getMath() + "\t"
+					+ teacher.getEnglish());
 		}
 	}
 }
