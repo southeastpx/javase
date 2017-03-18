89c0fe31545b9155b4d9e59018fa2d589a4eb55e
peng.xing <southeast_px@163.com>
2017-03-18 18:25:04 +0800

    映射文件以及直接缓冲区赋值文件

--- src/cn/pauu/javase/nio/TestChannel.java
+++ src/cn/pauu/javase/nio/TestChannel.java
@@ -4,7 +4,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import org.junit.Test;
 
@@ -12,6 +16,33 @@ import org.junit.Test;
  * 需求：利用通道和缓冲区复制文件
  */
 public class TestChannel {
+	// 直接缓冲区(简化版)
+	@Test
+	public void test3() throws IOException {
+		FileChannel inChannel = FileChannel.open(Paths.get("basa.jpg"), StandardOpenOption.READ);
+		FileChannel outChannel = FileChannel.open(Paths.get("ChannelCopy2.jpg"), StandardOpenOption.WRITE,
+				StandardOpenOption.CREATE);
+		inChannel.transferTo(0, inChannel.size(), outChannel);
+		outChannel.close();
+		inChannel.close();
+	}
+
+	// 直接缓冲区（内存映射文件）
+	@Test
+	public void test2() throws IOException {
+		FileChannel inChannel = FileChannel.open(Paths.get("basa.jpg"), StandardOpenOption.READ);
+		FileChannel outChannel = FileChannel.open(Paths.get("ChannelCopy.jpg"), StandardOpenOption.READ,
+				StandardOpenOption.WRITE, StandardOpenOption.CREATE_NEW);
+		// 内存映射文件（在物理内存中）
+		MappedByteBuffer inMappedBuf = inChannel.map(MapMode.READ_ONLY, 0, inChannel.size());
+		MappedByteBuffer outMappedBuf = outChannel.map(MapMode.READ_WRITE, 0, inChannel.size());
+		byte[] bys = new byte[1024];
+		inMappedBuf.get(bys);
+		outMappedBuf.put(bys);
+		outChannel.close();
+		inChannel.close();
+	}
+
 	// 非直接缓冲区
 	@Test
 	public void test1() {
