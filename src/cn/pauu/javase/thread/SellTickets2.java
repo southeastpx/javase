9ae84d40892dd95dd279ed868252993ac5b98c20
peng.xing <southeast_px@163.com>
2017-03-28 16:44:35 +0800

    同步代码块，同步方法，静态同步方法的锁

--- src/cn/pauu/javase/thread/SellTickets2.java
+++ src/cn/pauu/javase/thread/SellTickets2.java
@@ -1,20 +1,51 @@
 package cn.pauu.javase.thread;
 
+/*
+ * 1.同步代码块的锁是任意对象
+ * 2.同步方法的锁是this
+ * 3.静态同步方法的锁是当前类的字节码
+ */
 public class SellTickets2 implements Runnable {
-	private int tickets = 100;
+	private static int tickets = 100;
+	private Object obj = new Object();// 共享的锁
+	private Demo d = new Demo();
+	private int x = 0;
 
 	@Override
 	public void run() {
 		while (true) {
-			if (tickets > 0) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+			if (x % 2 == 0) {
+				synchronized (SellTickets2.class) {
+					if (tickets > 0) {
+						try {
+							Thread.sleep(300);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						System.out.println(Thread.currentThread().getName() + "正在出售第" + (tickets--) + "张电影票！");
+					}
 				}
-				System.out.println(Thread.currentThread().getName() + "正在出售第" + (tickets--) + "张电影票！");
+				x++;
+			} else {
+				sellTickets();
+				x++;
 			}
 		}
 	}
+
+	private static synchronized void sellTickets() {
+		if (tickets > 0) {
+			try {
+				Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + "正在出售第" + (tickets--) + "张电影票！");
+		}
+	}
+
+}
+
+class Demo {
 
 }
