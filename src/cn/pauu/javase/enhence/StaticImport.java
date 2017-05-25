74dda024b04bd58ad06651f40087b953fc508ea7
peng.xing <southeast_px@163.com>
2017-05-25 12:22:31 +0800

    Java基础加强复习，静态导入

---
+++ src/cn/pauu/javase/enhence/StaticImport.java
@@ -0,0 +1,10 @@
+package cn.pauu.javase.enhence;
+
+import static java.lang.Math.*;
+
+public class StaticImport {
+	public static void main(String[] args) {
+		System.out.println(max(3, 8));
+		System.out.println(abs(3 - 7));
+	}
+}
